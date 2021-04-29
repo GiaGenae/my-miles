35b32d62d97433b1182c0c4fbea02d7ef0c971af
Gia Jennings <gjennings.co@gmail.com>
2021-04-28 21:36:39 -0400

    Create migrations

--- db/migrate/20210428020526_create_miles.rb
+++ db/migrate/20210428020526_create_miles.rb
@@ -1,9 +1,10 @@
-class CreateMiles < ActiveRecord::Migration
+class CreateMiles < ActiveRecord::Migration[5.2]
   def change
     create_table :miles do |t|
       t.date :date
       t.decimal :distance
       t.time :duration
+      t.references :runner, foreign_key: {to_table: :users, on_delete: :cascade}
 
       t.timestamps null: false
     end
